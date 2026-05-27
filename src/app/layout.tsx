0cd4a5c3102982689f83149e23ee5da312780907
DimaShtempelyuk <dimon.shtempelyuk@gmail.com>
2026-05-27 20:36:23 +0200

    Add Google Analytics (G-XFXB7LKB9N)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import StyledComponentsRegistry from "@/lib/StyledComponentsRegistry";
 import "./globals.css";
 
@@ -53,6 +54,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       <body>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-XFXB7LKB9N" strategy="afterInteractive" />
+        <Script id="ga-init" strategy="afterInteractive">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-XFXB7LKB9N');
+        `}</Script>
       </body>
     </html>
   );
